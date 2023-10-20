21c9d3fa0f98052869340b0fe5f7df7bc0a40c67
wutongwutong00 <wu-tong-apple@hotmail.com>
2023-10-20 00:55:18 -0400

    Index Setup
    Getting link for Keratin

---
+++ index.js
@@ -0,0 +1,19 @@
+function showHair(){
+    document.getElementById("hair").style.display = "block";
+}
+function showSti(){
+    document.getElementById("sti").style.display = "block";
+}
+function showGrowth(){
+    document.getElementById("growth").style.display = "block";
+}
+function showDeath(){
+    document.getElementById("death").style.display = "block";
+}
+
+function keratin(){
+    var keratinWindow = window.open("", "keratin", "width=200,height=100");
+    // keratinWindow.document.write(
+    //     "<p>This is 'MsgWindow'. I am 200px wide and 100px tall!</p>"
+    //     );
+}
