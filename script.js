8347d02ffd8be041510cb7296ca0db001933f7b0
wutongwutong00 <wu-tong-apple@hotmail.com>
2023-10-27 01:25:03 -0400

    labhair update

--- script.js
+++ script.js
@@ -14,21 +14,25 @@ function showDeath(){
 function keratin(){
     var keratinWindow = window.open("https://wutongwutong00.github.io/thesis/keratin.html", "keratin", "width=300,height=200");
 }
-function keratin(){
-    var keratinWindow = window.open("https://wutongwutong00.github.io/thesis/keratin2.html", "keratin2", "width=300,height=200");
+function keratin2(){
+    var keratin2Window = window.open("https://wutongwutong00.github.io/thesis/keratin2.html", "keratin2", "width=300,height=200");
 }
 
 function cityHair(){
-    var keratinWindow = window.open("https://wutongwutong00.github.io/thesis/cityhair.html", "cityHair", "width=200,height=600");
+    var cityHairWindow = window.open("https://wutongwutong00.github.io/thesis/cityhair.html", "cityHair", "width=200,height=600");
 }
 function cytokinin(){
-    var keratinWindow = window.open("https://wutongwutong00.github.io/thesis/cytokinin.html", "cytokinin", "width=1920,height=1080");
+    var cytokininWindow = window.open("https://wutongwutong00.github.io/thesis/cytokinin.html", "cytokinin", "width=1920,height=1080");
 }
 function cytokinin2(){
-    var keratinWindow = window.open("https://wutongwutong00.github.io/thesis/cytokinin2.html", "cytokinin2", "width=1080,height=1080");
+    var cytokinin2Window = window.open("https://wutongwutong00.github.io/thesis/cytokinin2.html", "cytokinin2", "width=1080,height=1080");
 }
 function hairfall(){
-    var keratinWindow = window.open("https://wutongwutong00.github.io/thesis/hairfall/index.html", "hairfall", "width=257,height=859");
+    var hairfallWindow = window.open("https://wutongwutong00.github.io/thesis/hairfall/index.html", "hairfall", "width=257,height=859");
 }
+function labhair(){
+    var labHairWindow = window.open("https://wutongwutong00.github.io/thesis/labhair.html", "labhair", "width=300,height=300");
+}
+
 
 
